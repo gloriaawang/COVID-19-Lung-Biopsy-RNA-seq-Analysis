0a7dc39dfc47200a70f992429c2677f3f7d9d187
Gloria Wang <gw4164@gmail.com>
2024-12-05 00:09:21 -0500

    Update 1_kallisto_quantification.sh

--- code/1_kallisto_quantification.sh
+++ code/1_kallisto_quantification.sh
@@ -1,24 +1,13 @@
-#!/bin/bash
-
-# COVID-19 Lung Biopsy RNA-seq Analysis
 # Transcript Quantification Script
 
-# Set working directory and create output folders 
 WORKDIR=""
 cd $WORKDIR
 mkdir -p kallisto_index kallisto_output
-
-# Load required modules
 module load kallisto/0.46.1
-
-# Create kallisto index
-echo "Creating kallisto index..."
 kallisto index \
     -i kallisto_index/hg38_refMrna.idx \
     /hg38_refMrna.fa
 
-# Quantify uninfected samples
-echo "Processing uninfected samples..."
 
 # Uninfected sample 1
 kallisto quant \
@@ -35,7 +24,6 @@ kallisto quant \
     /SRR11517729.fastq.gz
 
 # Process pooled infected samples
-echo "Processing infected samples..."
 kallisto quant \
     -i kallisto_index/hg38_refMrna.idx \
     -o kallisto_output/Infected \
